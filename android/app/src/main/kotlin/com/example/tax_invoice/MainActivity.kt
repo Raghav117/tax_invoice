e60be130e3b3ceb87557e58d6f2a12368a70c133
raghavgarg <raghav.garg@convegenius.ai>
2025-05-11 16:20:36 +0530

    server side drive handling + flutter upgrade

--- android/app/src/main/kotlin/com/example/tax_invoice/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tax_invoice
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/tax_invoice_new/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.raghavg1999.inventory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
